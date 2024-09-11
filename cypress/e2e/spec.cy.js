7c5dfe2f2a0e9aea30ce6001916aa0ec3f7abe58
kobe.sin@hunghingprinting.com <46436626+kobesin322@users.noreply.github.com>
2024-09-12 05:21:29 +0800

    Test: test cases for fetching and toggle menu

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('IMDB Mockup e2e Testing', () => {
+  it('Fetching a page of movies', () => {
+
+    //XL Monoitor
+    cy.viewport(2560, 1294)
+
+    cy.visit('./index.html')
+    
+    cy.get(".gallery")
+    .find('.movie-poster-wrap')
+    .should("have.length", 20)
+
   })
+
+  it('Toggle popout menu', () => {
+
+    //Mobile
+    cy.viewport(375, 906)
+
+    cy.visit('./index.html')
+    
+    cy.get("#open-menu").click()
+
+    cy.get('.popout-menu.active')
+    .find('.breadcrumb_current')
+    .contains('Home')
+
+  })
+
+
 })
